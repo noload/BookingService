764b686e5aba205f1b76e85e27b1cf0e7f07f620
noload <vaibhavkamble023@gmail.com>
2024-01-05 14:44:10 +0530

    basic setup for booking service

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./route/index");
+const db = require("./models/index");
+
+const app = express();
+
+const setupAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use("/api", apiRoutes);
+
+  app.listen(PORT, () => {
+    console.log("server started on port " + PORT);
+
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alert: true });
+    }
+  });
+};
+
+setupAndStartServer();

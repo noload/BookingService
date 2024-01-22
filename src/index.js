f4046d67f21f5fe987b5f24e9a9d6cab428c5e1c
noload <vaibhavkamble023@gmail.com>
2024-01-22 19:39:28 +0530

    database setup completed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 const express = require("express");
+const podyParser = require("body-parser");
 const bodyParser = require("body-parser");
-const { PORT } = require("./config/serverConfig");
-const apiRoutes = require("./route/index");
-const db = require("./models/index");
-
 const app = express();
 
+const { PORT } = require("./config/serverConfig");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const setupAndStartServer = () => {
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use("/api", apiRoutes);
-
   app.listen(PORT, () => {
-    console.log("server started on port " + PORT);
-
-    if (process.env.DB_SYNC) {
-      db.sequelize.sync({ alert: true });
-    }
+    console.log(`server started at ${PORT}`);
   });
 };
 
